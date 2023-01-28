bc2a70bb1f91d290e530b1509d519039d7073dd5
Gris Ge <cnfourt@gmail.com>
2023-01-28 15:29:16 +0800

    Run cargo clippy
    Signed-off-by: Gris Ge <cnfourt@gmail.com>

--- src/parsers.rs
+++ src/parsers.rs
@@ -12,7 +12,7 @@ use crate::DecodeError;
 
 pub fn parse_mac(payload: &[u8]) -> Result<[u8; 6], DecodeError> {
     if payload.len() != 6 {
-        return Err(format!("invalid MAC address: {:?}", payload).into());
+        return Err(format!("invalid MAC address: {payload:?}").into());
     }
     let mut address: [u8; 6] = [0; 6];
     for (i, byte) in payload.iter().enumerate() {
@@ -23,7 +23,7 @@ pub fn parse_mac(payload: &[u8]) -> Result<[u8; 6], DecodeError> {
 
 pub fn parse_ipv6(payload: &[u8]) -> Result<[u8; 16], DecodeError> {
     if payload.len() != 16 {
-        return Err(format!("invalid IPv6 address: {:?}", payload).into());
+        return Err(format!("invalid IPv6 address: {payload:?}").into());
     }
     let mut address: [u8; 16] = [0; 16];
     for (i, byte) in payload.iter().enumerate() {
@@ -57,7 +57,7 @@ pub fn parse_ip(payload: &[u8]) -> Result<IpAddr, DecodeError> {
             payload[15],
         ])
         .into()),
-        _ => Err(format!("invalid IPv6 address: {:?}", payload).into()),
+        _ => Err(format!("invalid IPv6 address: {payload:?}").into()),
     }
 }
 
@@ -77,56 +77,56 @@ pub fn parse_string(payload: &[u8]) -> Result<String, DecodeError> {
 
 pub fn parse_u8(payload: &[u8]) -> Result<u8, DecodeError> {
     if payload.len() != 1 {
-        return Err(format!("invalid u8: {:?}", payload).into());
+        return Err(format!("invalid u8: {payload:?}").into());
     }
     Ok(payload[0])
 }
 
 pub fn parse_u32(payload: &[u8]) -> Result<u32, DecodeError> {
     if payload.len() != size_of::<u32>() {
-        return Err(format!("invalid u32: {:?}", payload).into());
+        return Err(format!("invalid u32: {payload:?}").into());
     }
     Ok(NativeEndian::read_u32(payload))
 }
 
 pub fn parse_u64(payload: &[u8]) -> Result<u64, DecodeError> {
     if payload.len() != size_of::<u64>() {
-        return Err(format!("invalid u64: {:?}", payload).into());
+        return Err(format!("invalid u64: {payload:?}").into());
     }
     Ok(NativeEndian::read_u64(payload))
 }
 
 pub fn parse_u128(payload: &[u8]) -> Result<u128, DecodeError> {
     if payload.len() != size_of::<u128>() {
-        return Err(format!("invalid u128: {:?}", payload).into());
+        return Err(format!("invalid u128: {payload:?}").into());
     }
     Ok(NativeEndian::read_u128(payload))
 }
 
 pub fn parse_u16(payload: &[u8]) -> Result<u16, DecodeError> {
     if payload.len() != size_of::<u16>() {
-        return Err(format!("invalid u16: {:?}", payload).into());
+        return Err(format!("invalid u16: {payload:?}").into());
     }
     Ok(NativeEndian::read_u16(payload))
 }
 
 pub fn parse_i32(payload: &[u8]) -> Result<i32, DecodeError> {
     if payload.len() != 4 {
-        return Err(format!("invalid u32: {:?}", payload).into());
+        return Err(format!("invalid u32: {payload:?}").into());
     }
     Ok(NativeEndian::read_i32(payload))
 }
 
 pub fn parse_u16_be(payload: &[u8]) -> Result<u16, DecodeError> {
     if payload.len() != size_of::<u16>() {
-        return Err(format!("invalid u16: {:?}", payload).into());
+        return Err(format!("invalid u16: {payload:?}").into());
     }
     Ok(BigEndian::read_u16(payload))
 }
 
 pub fn parse_u32_be(payload: &[u8]) -> Result<u32, DecodeError> {
     if payload.len() != size_of::<u32>() {
-        return Err(format!("invalid u32: {:?}", payload).into());
+        return Err(format!("invalid u32: {payload:?}").into());
     }
     Ok(BigEndian::read_u32(payload))
 }
