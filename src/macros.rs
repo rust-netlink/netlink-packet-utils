e4d4972fc6787b2cee898143ace229aef576a1f1
Tomasz Jankowski <tomasz.jankowski@gcore.com>
2025-03-19 08:44:31 +0000

    paste crate was abandoned, replace it with pastey
    Signed-off-by: Tomasz Jankowski <tomasz.jankowski@gcore.com>

--- src/macros.rs
+++ src/macros.rs
@@ -66,7 +66,7 @@ macro_rules! getter {
 macro_rules! setter {
     ($buffer: ident, $name:ident, slice, $offset:expr) => {
         impl<'a, T: AsRef<[u8]> + AsMut<[u8]> + ?Sized> $buffer<&'a mut T> {
-            $crate::paste::item! {
+            $crate::pastey::item! {
                 pub fn [<$name _mut>](&mut self) -> &mut [u8] {
                     &mut self.buffer.as_mut()[$offset]
                 }
@@ -79,14 +79,14 @@ macro_rules! setter {
         }
     };
     ($name:ident, u8, $offset:expr) => {
-        $crate::paste::item! {
+        $crate::pastey::item! {
             pub fn [<set_ $name>](&mut self, value: u8) {
                 self.buffer.as_mut()[$offset] = value;
             }
         }
     };
     ($name:ident, u16, $offset:expr) => {
-        $crate::paste::item! {
+        $crate::pastey::item! {
             pub fn [<set_ $name>](&mut self, value: u16) {
                 use $crate::byteorder::{ByteOrder, NativeEndian};
                 NativeEndian::write_u16(&mut self.buffer.as_mut()[$offset], value)
@@ -94,7 +94,7 @@ macro_rules! setter {
         }
     };
     ($name:ident, u32, $offset:expr) => {
-        $crate::paste::item! {
+        $crate::pastey::item! {
             pub fn [<set_ $name>](&mut self, value: u32) {
                 use $crate::byteorder::{ByteOrder, NativeEndian};
                 NativeEndian::write_u32(&mut self.buffer.as_mut()[$offset], value)
@@ -102,7 +102,7 @@ macro_rules! setter {
         }
     };
     ($name:ident, u64, $offset:expr) => {
-        $crate::paste::item! {
+        $crate::pastey::item! {
             pub fn [<set_ $name>](&mut self, value: u64) {
                 use $crate::byteorder::{ByteOrder, NativeEndian};
                 NativeEndian::write_u64(&mut self.buffer.as_mut()[$offset], value)
@@ -110,14 +110,14 @@ macro_rules! setter {
         }
     };
     ($name:ident, i8, $offset:expr) => {
-        $crate::paste::item! {
+        $crate::pastey::item! {
             pub fn [<set_ $name>](&mut self, value: i8) {
                 self.buffer.as_mut()[$offset] = value;
             }
         }
     };
     ($name:ident, i16, $offset:expr) => {
-        $crate::paste::item! {
+        $crate::pastey::item! {
             pub fn [<set_ $name>](&mut self, value: i16) {
                 use $crate::byteorder::{ByteOrder, NativeEndian};
                 NativeEndian::write_i16(&mut self.buffer.as_mut()[$offset], value)
@@ -125,7 +125,7 @@ macro_rules! setter {
         }
     };
     ($name:ident, i32, $offset:expr) => {
-        $crate::paste::item! {
+        $crate::pastey::item! {
             pub fn [<set_ $name>](&mut self, value: i32) {
                 use $crate::byteorder::{ByteOrder, NativeEndian};
                 NativeEndian::write_i32(&mut self.buffer.as_mut()[$offset], value)
@@ -133,7 +133,7 @@ macro_rules! setter {
         }
     };
     ($name:ident, i64, $offset:expr) => {
-        $crate::paste::item! {
+        $crate::pastey::item! {
             pub fn [<set_ $name>](&mut self, value: i64) {
                 use $crate::byteorder::{ByteOrder, NativeEndian};
                 NativeEndian::write_i64(&mut self.buffer.as_mut()[$offset], value)
